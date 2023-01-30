b3fe2586a87528a82e09194f177325c9ef95ec14
WFaustin <wf77@drexel.edu>
2023-01-30 18:28:07 -0500

    Navbar

--- src/routes/home.svelte
+++ src/routes/home.svelte
@@ -1,3 +1,31 @@
-<p>
-    WF Recordings Home Page
-</p>
+<script>
+    import TopNavbar from "./top-navbar.svelte";
+</script>
+<div id="head">
+    <TopNavbar></TopNavbar>
+</div>
+<div id="body">
+    <section id="titleSection">
+        <div>
+            <h1>WF Recordings</h1>
+            <p>
+                Committed to providing musical services all across the globe.
+            </p>
+        </div>
+
+    </section>
+    
+</div>
+
+<style>
+    *{
+        box-sizing: border-box;
+        font-family:Verdana, Geneva, Tahoma, sans-serif;
+        margin: 0px; 
+        padding: 0px;
+    }
+
+    p{
+        font-style: italic;
+    }
+</style>
---
+++ src/routes/top-navbar.svelte
@@ -0,0 +1,23 @@
+<nav>
+    <div id="logo">
+        WF
+    </div>
+    <div id="linkContainer">
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Contact Us</a>
+    </div>
+</nav>
+
+<style>
+    nav{
+        height: 70px; 
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        background-color: #56DAFC;
+    }
+    #linkContainer{
+        font-style: italic;
+    }
+</style>
